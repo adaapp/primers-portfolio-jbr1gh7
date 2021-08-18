751cfdf0d9503ce1d29aa3961b1440b0f4c581ad
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2021-08-18 08:43:35 +0000

    regex validation

--- include/monday.h
+++ include/monday.h
@@ -1,3 +1,5 @@
+#include <regex>
+
 void output(std::string message) {
     std::cout << message;
 }
@@ -18,22 +20,55 @@ float fToC(float fahrenheit) {
     return (fahrenheit - 32) * 5 / 9; //Fahrenheit to Centigrade.
 }
 
+bool validateNumber(std::string toBeValidated, bool isIntExpected = false) {
+    bool isValid = true;
+
+    std:: smatch matchLetters;
+    std::regex lettersExpression("[^0-9 \\.]"); //if chars other than 0-9 and the escaped literal '.' are present, then search returns true
+
+    if (std::regex_search(toBeValidated, matchLetters, lettersExpression)) {
+        isValid = false;
+    }
+
+    std::regex decimalsExpression("\\."); //match only '.'
+
+    std::ptrdiff_t occurences(std::distance(
+        std::sregex_iterator(toBeValidated.begin(), toBeValidated.end(), decimalsExpression),
+        std::sregex_iterator())
+    ); //iterate over all matches of '.' and total all occurences with std::distance
+
+    if (!isIntExpected) {
+        if (occurences > 1) { //no more than 1 decimal for float
+            isValid = false;
+        }
+    }
+    else {
+        if (occurences > 0) { //no decimals allowed for int
+            isValid = false;
+        }
+    }
+
+    if (!isValid && !isIntExpected) {
+        output("ERROR: \"" + toBeValidated + "\" is not a valid decimal number. Try again.");
+    }
+    else if (!isValid && isIntExpected) {
+        output("ERROR: \"" + toBeValidated + "\" is not a valid whole number. Try again.");
+    }
+
+    return isValid;
+}
+
 void fahrenheitCentigradeConversion(void) {
     bool retryChoice = true;
-    std::string temperatureString;
     float temperatureFloat;
+    std::string temperatureString;
 
     while (retryChoice) {
         temperatureString = promptUserInput("\nPlease enter the starting temperature: ");
 
-        try {
-            stof(temperatureString);
+        if (validateNumber(temperatureString)) {
             temperatureFloat = stof(temperatureString);
             retryChoice = false;
-
-        }
-        catch (const std::invalid_argument) {
-            output("ERROR: \"" + temperatureString + "\" is not digits. Try again.");
         }
     }
 
@@ -66,20 +101,40 @@ void selfServiceCheckout(void) {
     int itemCounter = 0;
     float cumulativeSum = 0.0;
     float currentCost;
+    bool retryChoice = true;
+    std::string quantityString;
+    std::string currentCostString;
 
 	while (quantity > 0) {
         itemCounter ++;
-        quantity = stoi(promptUserInput("\nPlease enter a quantity for item " + std::to_string(itemCounter) + " (or 0 to finish): "));
-        
+
+        while (retryChoice) {
+            quantityString = promptUserInput("\nPlease enter a quantity for item " + std::to_string(itemCounter) + " (or 0 to finish): ");
+            if (validateNumber(quantityString, true)) {
+                quantity = std::stoi(quantityString);
+                retryChoice = false;
+            }
+        }
+
+        retryChoice = true;
+
         if (quantity > 0) {
-            currentCost = stof(promptUserInput("\nPlease enter item " + std::to_string(itemCounter) + "’s cost: "));
-            cumulativeSum += quantity * currentCost;
+            while (retryChoice) {
+                currentCostString = promptUserInput("\nPlease enter item " + std::to_string(itemCounter) + "’s cost: ");
+                if (validateNumber(currentCostString)) {
+                    currentCost = std::stof(currentCostString);
+                    cumulativeSum += quantity * currentCost;
+                    retryChoice = false;
+                }
+            }
         }
         else {
             float shoppingTax = cumulativeSum * (taxRate / 100);
             output("\nSubtotal: £" + std::to_string(cumulativeSum) + "\nShopping tax: £" + std::to_string(shoppingTax) + "\n");
             output("\nTotal: £" + std::to_string(cumulativeSum + shoppingTax)); 
         }
+
+        retryChoice = true;
     }
 }
 
