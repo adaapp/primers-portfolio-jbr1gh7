07343b3276bc7fb1d7156d9143769be46ee5e88f
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2021-08-19 23:03:22 +0000

    reads data, pushes to vector

--- include/wednesday.h
+++ include/wednesday.h
@@ -1,7 +1,27 @@
-void phoneDirectory(void) {
-	std::cout << " - phoneDirectory: not yet implemented\n\n";
+#include <fstream>
+#include <vector>
+
+std::vector<std::string> readFile() {
+    std::ifstream file("include/contacts.csv");
+
+    std::string line;
+    std::vector<std::string> entries;
+
+    while(getline(file, line)) {
+        entries.push_back(line);
+        output("\n" + line);
+    }
+
+    file.close();
+    return entries;
 }
 
+void phoneDirectory(void) {
+    std::string search = promptUserInput("\nEnter a name or number to search: ");
+
+    std::vector<std::string> data = readFile(); 
+
+}
 
 void dataFileParser(void) {
 	std::cout << " - dataFileParser: not yet implemented\n\n";
