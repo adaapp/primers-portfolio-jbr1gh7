55510f97da66077df6bd6640078ec95cb68ee60c
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2021-08-19 23:40:35 +0000

    case insensitive regex working correctly, can search for 'ada' and it will fetch 'Ada College...'. Can also search for '496' and it will fetch the 5 entries with phone number containing 496.

--- include/wednesday.h
+++ include/wednesday.h
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <vector>
+#include <regex>
 
 std::vector<std::string> readFile() {
     std::ifstream file("include/contacts.csv");
@@ -12,15 +13,38 @@ std::vector<std::string> readFile() {
         output("\n" + line);
     }
 
+    output("\n");
     file.close();
     return entries;
 }
 
 void phoneDirectory(void) {
     std::string search = promptUserInput("\nEnter a name or number to search: ");
-
+    std::regex searchExpression(search, std::regex_constants::icase);
+    std::sregex_iterator searchIterator;
     std::vector<std::string> data = readFile(); 
+    
+    bool isFound = false;
 
+    for (int i=0; i<data.size(); i++) {
+        searchIterator = std::sregex_iterator(data[i].begin(), data[i].end(), searchExpression);
+        std::ptrdiff_t foundCount(std::distance(
+            searchIterator,
+            std::sregex_iterator())
+        );
+        
+        //output("\n foundCount: " + std::to_string(foundCount));
+        if (foundCount > 0) {
+            for (std::sregex_iterator j=searchIterator; j!=std::sregex_iterator(); j++) {
+                output("\nfound: " + data[i]);
+                isFound = true;
+            }
+        }
+    }
+    
+    if (!isFound) {
+        output("\nSorry, no contact details found"); 
+    }
 }
 
 void dataFileParser(void) {
