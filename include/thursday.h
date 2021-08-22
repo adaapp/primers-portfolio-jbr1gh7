096aa45744dbdfbb0b976251c553507dbf0efb23
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2021-08-22 02:54:50 +0000

    uses lexical cast to cast thread to string and output, detach threads

--- include/thursday.h
+++ include/thursday.h
@@ -1,38 +1,52 @@
 #include<thread>
+#include <boost/lexical_cast.hpp>
 
 int thread1Time = 5;
 int thread2Time = 10;
 
 void sleepTimer(int seconds = 5) {
-	std::this_thread::sleep_for(std::chrono::milliseconds(seconds * 1000));
-    output("I am back awake!");
+	std::this_thread::sleep_for(std::chrono::seconds(seconds));
 }
 
+//creates a thread and sleeps it for the pre-defined duration
 void timer1() {
-    output("\nThread 1: " + std::this_thread::get_id() + " started");
+    output("\nThread 1: " + boost::lexical_cast<std::string>(std::this_thread::get_id()) + " started");
     sleepTimer(thread1Time);
-    output("\nThread 1: " + std::this_thread::get_id() + " ended");
+    output("\nThread 1: " + boost::lexical_cast<std::string>(std::this_thread::get_id()) + " ended");
 }
 
 void timer2() {
-    output("\nThread 2: " + std::this_thread::get_id() + " started");
+    output("\nThread 2: " + boost::lexical_cast<std::string>(std::this_thread::get_id()) + " started");
     sleepTimer(thread2Time);
-    output("\nThread 2: " + std::this_thread::get_id() + " ended");
+    output("\nThread 2: " + boost::lexical_cast<std::string>(std::this_thread::get_id()) + " ended");
 }
 
-std::this_thread joinThreads(void) {
+void joinThreads(void) {
+    //define the two threads in parallel
     std::thread thread1(timer1);
     std::thread thread2(timer2);
 
-    output("\nMain thread: " + std::this_thread::get_id() + "started");
+    output("\nMain thread: " + boost::lexical_cast<std::string>(std::this_thread::get_id()) + " started");
 
     thread1.join();
     thread2.join();
 
-    output("\nMain thread: " + std::this_thread::get_id() + "ended");
-    return std::this_thread::get_id()
+    output("\nMain thread: " + boost::lexical_cast<std::string>(std::this_thread::get_id()) + " ended");
+}
+
+void detachThreads(void) {
+    std::thread thread1(timer1);
+    std::thread thread2(timer2);
+
+    output("\nMain thread: " + boost::lexical_cast<std::string>(std::this_thread::get_id()) + " started");
+
+    thread1.detach();
+    thread2.detach();
+
+    output("\nMain thread: " + boost::lexical_cast<std::string>(std::this_thread::get_id()) + " ended");
 }
 
 void joinDetachThreads(void) {
 	joinThreads();
+    detachThreads();
 }
