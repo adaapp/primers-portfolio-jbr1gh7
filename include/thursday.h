ecaf111f0bff4f01f6e01bd9a498fe06665c2413
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2021-08-22 02:08:03 +0000

    added thread timers

--- include/thursday.h
+++ include/thursday.h
@@ -1,11 +1,30 @@
 #include<thread>
 
+int thread1Time = 5;
+int thread2Time = 10;
+
 void sleepTimer(int seconds = 5) {
 	std::this_thread::sleep_for(std::chrono::milliseconds(seconds * 1000));
     output("I am back awake!");
 }
 
+void timer1() {
+    output("\nThread 1: " + std::this_thread::get_id() + " started");
+    sleepTimer(thread1Time);
+    output("\nThread 1: " + std::this_thread::get_id() + " ended");
+}
+
+void timer2() {
+    output("\nThread 2: " + std::this_thread::get_id() + " started");
+    sleepTimer(thread2Time);
+    output("\nThread 2: " + std::this_thread::get_id() + " ended");
+}
+
+int join(void) {
+    output("Main thread: " + std::this_thread::get_id() + "started");
+    
+}
 
 void joinDetachThreads(void) {
-	std::cout << " - joinDetachThreads: not yet implemented\n\n";
+	
 }
