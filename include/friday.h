ce1246f8cf480a9714a7c14eb12e54f8fd593925
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2021-08-23 22:43:50 +0000

    added private properties and public methods

--- include/friday.h
+++ include/friday.h
@@ -1,5 +1,70 @@
+class Car {
+    private:
+        std::string colour;
+        std::string make;
+        bool isEngineOn;
+        bool isCarLocked;
+    
+    public: 
+        void set_colour(std::string) {
+
+        }
+
+        std::string get_colour(void) {
+
+        }
+
+        void set_make(std::string) {
+
+        }
+
+        std::string get_make(void) {
+
+        }
+        
+        void engine_on(void) {
+
+        }
+
+        void engine_off(void) {
+
+        }
+        
+        void locked(bool) {
+
+        }
+
+        void status(void) {
+            
+        }
+};
+
 void carClass(void) {
-		std::cout << " - carClass: not yet implemented\n\n";
+    /*
+    Car Status: colour: Blue, make: Honda, engine: Off, Unlocked
+    1. Turn Engine On
+    2. Turn Engine Off
+    3. Lock Car
+    4. Unlock Car
+    Please select an option (or 0 to finish): 2
+    Sorry, the engine is already off
+    Car Status: colour: Blue, make: Honda, engine: Off, Unlocked
+    1. Turn Engine On
+    2. Turn Engine Off
+    3. Lock Car
+    4. Unlock Car
+    Please select an option (or 0 to finish): 1
+    Turning the engine on
+    Car Status: colour: Blue, make: Honda, engine: Running, Unlocked
+    1. Turn Engine On
+    2. Turn Engine Off
+    3. Lock Car
+    4. Unlock Car
+    Please select an option (or 0 to finish): 4
+    Sorry, the car is already unlocked
+    */
+
+    
 }
 
 
